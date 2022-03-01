77c724d32e04767a27578d78c5bafaa86ddfcf26
qzart45hi <qwert45hi@yandex.ru>
2022-03-01 21:32:30 +0300

    feat: main carousel navigation

---
+++ icons/arrows.tsx
@@ -0,0 +1,15 @@
+export function LargeForwardNavigationArrow() {
+  return <div>hey</div>
+}
+
+export function LargeBackwardNavigationArrow() {
+  return <div>yeh</div>
+}
+
+export function SmallForwardNavigationArrow() {
+  return <div>hey (smol)</div>
+}
+
+export function SmallBackwardNavigationArrow() {
+  return <div>)loms( yeh</div>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,57 @@
-import { Stack } from "@mui/material"
+import { IconButton, Stack } from "@mui/material"
+import { useState } from "react"
 import { DualColourSpan } from "../components/lib/styling"
+import { LargeBackwardNavigationArrow, LargeForwardNavigationArrow } from "../icons/arrows"
 
-function MainCarousel() {
-  return <div style={{ position: "relative" }}>
+interface MainCarouselData {
+  image: string
+  title: string
+  url: string
+}
+
+interface MainCarouselProps {
+  data: MainCarouselData[]
+}
+
+function MainCarousel({ data }: MainCarouselProps) {
+  const [position, setPosition] = useState(0)
+  const current = data[position]
+
+  return <div style={{ position: "relative", overflowX: "hidden" }}>
     <img
-      src="https://filmatik.ru/uploads/background/original/banner2.jpg"
+      src={current.image}
       alt="alt"
       style={{ objectFit: "cover", width: "100%", height: 700, objectPosition: "100% 0" }}
     />
     <span style={{ position: "absolute", color: "#555", bottom: "2%", right: "2%", margin: 0, fontSize: 18 }}>
-      Гнев Человеческий
+      {current.title}
     </span>
+    <IconButton
+      disableRipple
+      onClick={() => setPosition((position + data.length - 1) % data.length)}
+      style={{
+        position: "absolute",
+        bottom: "50%",
+        right: "1%",
+        margin: 0,
+        background: "transparent",
+      }}
+    >
+      <LargeForwardNavigationArrow />
+    </IconButton>
+    <IconButton
+      disableRipple
+      onClick={() => setPosition((position + 1) % data.length)}
+      style={{
+        position: "absolute",
+        bottom: "50%",
+        left: "1%",
+        margin: 0,
+        background: "transparent",
+      }}
+    >
+      <LargeBackwardNavigationArrow />
+    </IconButton>
   </div>
 }
 
@@ -18,14 +59,27 @@ function FilmCarousel() {
   return <div></div>
 }
 
+const mainCarouselViewData: MainCarouselData[] = [
+  {
+    image: "https://filmatik.ru/uploads/background/original/banner2.jpg",
+    title: "Гнев Человеческий",
+    url: "",
+  },
+  {
+    image: "https://filmatik.ru//uploads/background/original/banner3.jpg",
+    title: "Армия Мертвецов",
+    url: "",
+  },
+]
+
 export default function Home() {
   return <Stack direction="column" style={{ alignContent: "center", justifyContent: "center" }}>
-    <MainCarousel />
-      <div style={{ textAlign: "center", fontSize: 34 }}>
-        <DualColourSpan whiteText="Топ" orangeText="премьер" />
-      </div>
-      <Stack direction="column">
-    <FilmCarousel />
+    <MainCarousel data={mainCarouselViewData} />
+    <div style={{ textAlign: "center", fontSize: 34 }}>
+      <DualColourSpan whiteText="Топ" orangeText="премьер" />
+    </div>
+    <Stack direction="column">
+      <FilmCarousel />
     </Stack>
   </Stack>
 }
